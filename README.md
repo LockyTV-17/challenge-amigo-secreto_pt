c80d72ea4c48cb7f72999f9b359968dfdf46f064
onerd <onerd170@gmnail.com>
2025-01-24 00:57:05 -0300

    v1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+JOGO DO AMIGO SECRETO
+
+Neste jogo, o usuário pode adicionar amigos a uma lista e, em seguida, sortear um vencedor.
+
+O usuário pode adicionar os nomes clicando no botão "Adicionar".
+Após adicionar os nomes, o usuário pode realizar o sorteio clicando em "Sortear Amigo".
+
+Regras de validação:
+Se o campo de entrada estiver vazio, o usuário receberá uma mensagem de aviso ao tentar adicionar um nome.
+Caso o usuário tente realizar o sorteio sem adicionar nenhum nome à lista, ele também receberá uma mensagem de aviso.
+
+
+Feito Por: Luan Do Amparo Pereira.
+
+
+Meu git: https://github.com/LockyTV-17
+Meu linkedin: https://www.linkedin.com/in/luan-amparo-427409ba/
--- app.js
+++ app.js
@@ -1,1 +1,47 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    const addAmigo = document.getElementById('amigo');
+
+
+if (addAmigo.value.trim()) {
+    amigos.push(addAmigo.value.trim()); 
+    atualizarLista(); 
+    addAmigo.value = ""; 
+} else {
+    alert ("Por favor, insira um nome.");
+};
+
+
+};
+
+function atualizarLista(){
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+
+amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent  = amigo;
+    listaAmigos.appendChild(li);
+
+})
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("A lista está vazia. Adicione amigos antes de sortear!");
+        return;
+    }
+
+    const indiceAleatorio = Math.floor(Math.random()*amigos.length);
+    
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<p>O amigo sorteado foi: <strong>${amigoSorteado}</strong></p>`;
+    
+    amigos = []; 
+    atualizarLista();
+}
